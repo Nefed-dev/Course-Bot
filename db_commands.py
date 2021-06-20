0b97eb25979016b00a6aeea8952d91f3450a3c7e
nefedov-it <69164416+nefedov-it@users.noreply.github.com>
2021-06-20 21:10:15 +0300

    Create db commands file

---
+++ db_commands.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+# @sync_to_async
+def get_categories() -> List:
+    pass
+
+
+def get_subcategories() -> List:
+    pass
+
+
+def count_items():
+    pass
+
+
+def get_items() -> List:
+    pass
+
+
+def get_item() -> List:
+    pass
