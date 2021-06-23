33970df9dad8e8e7c885531bfa54dfee4a3afd23
nefedov-it <69164416+nefedov-it@users.noreply.github.com>
2021-06-23 06:54:13 +0500

    Untermediate update

--- db_commands.py
+++ db_commands.py
@@ -1,22 +1,24 @@
 from typing import List
 
+from asgiref.sync import sync_to_async
+from admin_panel.usersmanage.models import Item, User
 
-# @sync_to_async
+@sync_to_async
 def get_categories() -> List:
     pass
 
-
+@sync_to_async
 def get_subcategories() -> List:
     pass
 
-
+@sync_to_async
 def count_items():
     pass
 
-
+@sync_to_async
 def get_items() -> List:
     pass
 
-
+@sync_to_async
 def get_item() -> List:
     pass
